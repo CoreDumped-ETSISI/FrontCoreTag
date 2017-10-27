fa6ea0eb36ea0d75991b92d863d41b9d11e1b95f
Carlos Gonzalez <carlosgj94@gmail.com>
2017-10-27 21:12:58 +0200

    Added the header component

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+<nav>
+  <div class="nav-wrapper pink lighten-3">
+    <span class="brand-logo">Laser Tag</span>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp'
+}
+</script>
