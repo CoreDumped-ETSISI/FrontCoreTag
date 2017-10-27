95d56575b8c9ae2727c3f98a99d0ee2b6305c5f4
dmerinero <davidmerineroharo@gmail.com>
2017-10-28 00:30:24 +0200

    Trying to provide dinamically types of match

---
+++ src/components/MatchType.vue
@@ -0,0 +1,27 @@
+<template>
+<nav>
+  <div>
+    <span>Type of match:</span>
+	<div class="input-field col s12">
+		<select>
+		    <option value="" disabled selected>Choose a match type</option>
+	            <option v-for="(type, index) in types" value={{index}}>
+		   	{{type}} 
+		    </option>
+		</select>
+		<label>Materialize Select</label>
+	</div>
+  </div>
+</nav>
+</template>
+
+<script>
+	export default {
+		name: 'Type',
+		data: {
+			return:{
+				types: [ message: 'FREE-4-ALL' ]
+			}
+		}
+	}
+</script>
