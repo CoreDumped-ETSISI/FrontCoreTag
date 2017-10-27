ead01f1f9fd3eeb0dc84e584d6fc3de205a0d6c2
cesaramnuel <cesitar.miko@gmail.com>
2017-10-27 23:51:04 +0200

    First components created
    NewPlayer component added, empty Match component added

---
+++ src/components/IntroCenter.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div class="buttonContainer">
+    <a class="waves-effect waves-light btn-large" v-on:click="toNewPlayer">New Player</a>
+    <a class="waves-effect waves-light btn-large" v-on:click="toMatch">Match!</a>
+</div>
+</template>
+
+<script>
+    export default {
+        name:'IntroCenter',
+        methods: {
+            toNewPlayer (event) {
+                console.log('New player requested;')
+                this.$parent.$emit('ChangeView', 'NewPlayer');
+            },
+            toMatch (event) {
+                console.log('New player requested;')
+                this.$parent.$emit('ChangeView', 'Match');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .buttonContainer{
+        margin-top: 30%;
+    }
+</style>
