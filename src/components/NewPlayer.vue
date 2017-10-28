2b32cc775cc4bb8b1edb8548d6fae6b4ff150a4d
cesaramnuel <cesitar.miko@gmail.com>
2017-10-28 04:57:33 +0200

    Post method to createUser added.
    Pending testing (no API uploaded when this method was written).

--- src/components/NewPlayer.vue
+++ src/components/NewPlayer.vue
@@ -5,7 +5,7 @@
     <form class="col s12">
       <div class="row">
         <div class="col s12">
-          Enter desired nickname. Coolest ones rocks!:
+          Enter desired nickname. Make sure you pick a cool one!:
           <div class="input-field inline">
             <input id="nickname" type="text" class="validate">
             <label>Nickname</label>
@@ -20,21 +20,27 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+
     name: 'NewPlayer',
         methods: {
             createPlayer (event) {
                 console.log('New player sign up requested;')
+                var nick = document.getElementById("nickname").value
                 //HTTP request must be done here. And below line must be placed only when 
                 //request success (responseCode = 200).
-                /*
-                this.$http.get('ROUTE', function(data, status, response) {
-                this.users = data;
-            }).error(function (data, status, request) {
-                // handle error
-            }); */
-                this.$parent.$emit('ChangeView', 'IntroCenter');
-            }
+                axios.post('http://192.168.0.25:3000/api/createUser', 
+                    {"nickname": "" + nick})
+                    .then(response => {
+                    if(response.data == 200) {
+                        this.$parent.$emit('ChangeView', 'IntroCenter');
+                    }
+                }).catch(e => {
+                    console.log(e)
+                });
+}
         }
 }
 </script>
