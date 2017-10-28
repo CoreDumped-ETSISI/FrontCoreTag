acaa03f35902fac1e2a7507b174c4a371bd5d8b6
Carlos Gonzalez <carlosgj94@gmail.com>
2017-10-28 07:48:46 +0200

    Added deaths and made the labels bigger

--- src/components/PlayerLifes.vue
+++ src/components/PlayerLifes.vue
@@ -1,24 +1,38 @@
 <template>
-	<div class="row">
-		<div class="col s6 right-align" v-for="life in lifes">
-			x{{ life.message }}
-		</div>
-		<div class="col s6 left-align">
-			<img src="../images/heart.png" style="max-width: 2em">
-		</div>
-      	</div>
+    <div class="row">
+        <div class="col s8 right-align lifes">
+            <h1>Lives:<b class="extra-lifes"> x{{ lifes }}</b>
+            <img src="../images/heart.png" style="max-width: 2em"/>
+            </h1>
+        </div>
+        <div class="col s8 right-align lifes">
+            <h1>Deaths:<b class="extra-lifes"> x{{ deaths }}</b>
+            <img src="../images/Skull.png" style="max-width: 2em"/>
+            </h1>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Lifes',
     data: function() {
-	return {
-		page: 'IntroCenter',
-		lifes: [
-			{ message: 3 }
-		]
-	}
+        return {
+            page: 'IntroCenter',
+            lifes: 3,
+            deaths: 10
+        }
     }
 }
 </script>
+
+<style>
+.lifes{
+    font-size: xx-large;
+    font-size: 350%;
+}
+.extra-lifes{
+    font-size: 200%;
+}
+
+</style>
