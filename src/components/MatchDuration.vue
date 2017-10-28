e90e3fb3b93f4dbde0394c1feaffadfdfd4e23d0
Carlos Gonzalez <carlosgj94@gmail.com>
2017-10-28 02:14:20 +0200

    Duration Component done and sending the data to the parent

--- src/components/MatchDuration.vue
+++ src/components/MatchDuration.vue
@@ -1,29 +1,58 @@
 <template>
     <div>
-    <label>Duration:</label>
-      <!-- Switch -->
-  <div class="switch">
-    <label>
-      Off
-      <input type="checkbox" v-on:click="durationSwitch">
-      <span class="lever"></span>
-      On
-    </label>
-  </div>
-  </div>
+        <label>Duration:</label>
+        <!-- Switch -->
+        <div class="switch">
+            <label>
+                Infinite
+                <input type="checkbox" v-on:click="durationSwitch">
+                <span class="lever"></span>
+                Finite
+            </label>
+        </div>
+        <div v-if="switchState == 'Finite'">
+            <br>
+             <form action="#">
+            <p>
+            <input name="group1" type="radio" id="5" v-on:click="radioTime"/>
+            <label for="5">5 mins</label>
+            </p>
+            <p>
+            <input name="group1" type="radio" id="10" v-on:click="radioTime"/>
+            <label for="10">10 mins</label>
+            </p>
+             </form>
+        </div>
+
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Duration',
+    data: function(){
+        return {
+            switchState: 'Infinite'
+        }
+    },
     methods: {
         durationSwitch (event) {
-            if(event == true){
-                console.log("True");
+            //We get the switch state
+            var state = event.target.checked;
+            if(state == true){
+                //Set the DOM variable data
+                this.switchState = 'Finite';
+                this.$parent.$emit('duration', 'finite');
             }else{
-                console.log('false');
+                //Set the DOM variable data
+                this.switchState = 'Infinite';
+                this.$parent.$emit('duration', 'infinite');
             }
         },
+        radioTime (event) {
+            console.log(event.target.id);
+            this.$parent.$emit('durationTime', event.target.id);
+        }
     }
 }
 </script>
